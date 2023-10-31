e36c24705c847b155f48cb4a01df67d0b1afa366
Reis <1623971569121002@mil>
2023-10-31 16:58:16 -0600

    add 3D plot with slider

--- utils.py
+++ utils.py
@@ -778,110 +778,106 @@ def plot_3D_settlement(settlementStart, beamInfo3D):
 
 # 3D Plot - settlement 
 def plot_3D_settlement_slider(settlementStart, beamInfo3D):
-fig = go.Figure()
+    fig = go.Figure()
 
-for col in settlementStart.columns:
-    # Plot the beam locations as lines
-    for (startX, endX, startY, endY, startZ, endZ) in zip(beamInfo3D['startX'], beamInfo3D['endX'], 
-                                                          beamInfo3D['startY'], beamInfo3D['endY'], 
-                                                          beamInfo3D['{0}_start'.format(col)], 
-                                                          beamInfo3D['{0}_end'.format(col)]):
-        fig.add_trace(go.Scatter3d(
-            x=[startX, endX],
-            y=[startY, endY],
-            z = [startZ, endZ],
-            text = beamInfo3D['MP_W_S'],
-            #name="",
-            mode='lines',
-            line = dict(
-                color = 'black',
-                width = 1.5,
-                dash = 'solid'),
-            #hoverinfo='skip',
-            showlegend=False, 
-            #setting only the first dataframe to be visible as default
-            visible = (col==settlementStart.columns[len(settlementStart.columns)-1])))
-    
-    # Plot the Marker Point (MP) labels in grey
-        fig.add_trace(go.Scatter3d(
-            x=beamInfo3D['labelX'],
-            y=beamInfo3D['labelY'],
-            z=beamInfo3D['{0}_start'.format(col)],
-            text=beamInfo3D['MP_W_S'],
-            mode = 'text',
-            textfont = dict(
-                size = 10,
-                color = 'grey'),
-            hoverinfo='skip',
-            showlegend=False, 
-            #setting only the first dataframe to be visible as default
-            visible = (col==settlementStart.columns[len(settlementStart.columns)-1])))
+    for col in settlementStart.columns:
+        # Plot the beam locations as lines
+        for (startX, endX, startY, endY, startZ, endZ) in zip(beamInfo3D['startX'], beamInfo3D['endX'], 
+                                                            beamInfo3D['startY'], beamInfo3D['endY'], 
+                                                            beamInfo3D['{0}_start'.format(col)], 
+                                                            beamInfo3D['{0}_end'.format(col)]):
+            fig.add_trace(go.Scatter3d(
+                x=[startX, endX],
+                y=[startY, endY],
+                z = [startZ, endZ],
+                text = beamInfo3D['MP_W_S'],
+                #name="",
+                mode='lines',
+                line = dict(
+                    color = 'black',
+                    width = 1.5,
+                    dash = 'solid'),
+                #hoverinfo='skip',
+                showlegend=False, 
+                #setting only the first dataframe to be visible as default
+                visible = (col==settlementStart.columns[len(settlementStart.columns)-1])))
         
-fig.update_traces(
-    hovertemplate="<br>".join([
-        "MP: %{text}",
-        "Settlement [in]: %{z}",
-    ])
-)
-    
-fig.update_scenes(xaxis_autorange="reversed", 
-                  yaxis_autorange="reversed",
-                  zaxis_autorange="reversed")  
+        # Plot the Marker Point (MP) labels in grey
+            fig.add_trace(go.Scatter3d(
+                x=beamInfo3D['labelX'],
+                y=beamInfo3D['labelY'],
+                z=beamInfo3D['{0}_start'.format(col)],
+                text=beamInfo3D['MP_W_S'],
+                mode = 'text',
+                textfont = dict(
+                    size = 10,
+                    color = 'grey'),
+                hoverinfo='skip',
+                showlegend=False, 
+                #setting only the first dataframe to be visible as default
+                visible = (col==settlementStart.columns[len(settlementStart.columns)-1])))
+            
+    fig.update_traces(
+        hovertemplate="<br>".join([
+            "MP: %{text}",
+            "Settlement [in]: %{z}",
+        ])
+    )
+        
+    fig.update_scenes(xaxis_autorange="reversed", 
+                    yaxis_autorange="reversed",
+                    zaxis_autorange="reversed")  
 
-camera = dict(
-    up=dict(x=0, y=0, z=1),
-    center=dict(x=0, y=0, z=0),
-    eye=dict(x=1.5, y=1.5, z=1.5)
-)
-
-fig.update_layout(
-    autosize=False,
-    width=800, 
-    height=450,
-    margin=dict(l=0, r=0, b=0, t=0),
-    scene_camera=camera,
-    scene=dict(
-        xaxis_title='',
-        yaxis_title='',
-        zaxis_title='Cumulative Settlement [ft]',
-    ),
-)
-
-# groups and trace visibilities
-vis = []
-visList = []
-
-for  i, col in enumerate(settlementStart.columns):
-    n = len(beamInfo3D.index)*2
-    vis = ([False]*i*n + [True]*n + [False]*(len(settlementStart.columns)-(i+1))*n)
-    visList.append(vis)
-    vis = []
-
-
-# buttons for each group
-steps = []
-for idx, col in enumerate(settlementStart.columns):
-    steps.append(
-        dict(
-            label = col,
-            method = "update",
-            args=[{"visible": visList[idx]}])
+    camera = dict(
+        up=dict(x=0, y=0, z=1),
+        center=dict(x=0, y=0, z=0),
+        eye=dict(x=1.5, y=1.5, z=1.5)
     )
 
-# steps = [{'label': 'Select Survey Date',
-#                  'method': 'restyle',
-#                  'args': ['visible', [False]*len(settlementStart.columns)*len(beamInfo3D.index)]}] + steps
+    fig.update_layout(
+        autosize=False,
+        width=800, 
+        height=450,
+        margin=dict(l=0, r=0, b=0, t=0),
+        scene_camera=camera,
+        scene=dict(
+            xaxis_title='',
+            yaxis_title='',
+            zaxis_title='Cumulative Settlement [ft]',
+        ),
+    )
 
-sliders = [dict(
-    active=len(settlementStart.columns)-1,
-    currentvalue={"prefix": "Survey Date: "},
-    pad={"t": 50, "b":50},
-    steps=steps
-)]
+    # groups and trace visibilities
+    vis = []
+    visList = []
 
-fig.update_layout(
-    sliders=sliders,
-    width = 700,
-    height = 700
-)
+    for  i, col in enumerate(settlementStart.columns):
+        n = len(beamInfo3D.index)*2
+        vis = ([False]*i*n + [True]*n + [False]*(len(settlementStart.columns)-(i+1))*n)
+        visList.append(vis)
+        vis = []
+
+
+    # buttons for each group
+    steps = []
+    for idx, col in enumerate(settlementStart.columns):
+        steps.append(
+            dict(
+                label = col,
+                method = "update",
+                args=[{"visible": visList[idx]}])
+        )
+
+    sliders = [dict(
+        active=len(settlementStart.columns)-1,
+        currentvalue={"prefix": "Survey Date: "},
+        pad={"t": 50, "b":50},
+        steps=steps
+    )]
+
+    fig.update_layout(
+        sliders=sliders,
+        width = 700,
+        height = 700
+    )
     return fig

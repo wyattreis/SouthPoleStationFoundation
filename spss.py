5a3b830803818ce4eb1535ce1badf2f4f652125f
Reis <1623971569121002@mil>
2023-10-31 10:10:57 -0600

    change app banner image

--- spss.py
+++ spss.py
@@ -21,12 +21,11 @@ from utils import *
 
 st.set_page_config(layout="wide")
 
-left_co, cent_co,last_co = st.columns([0.1, 0.8, 0.1])
+left_co, cent_co,last_co = st.columns([0.05, 0.9, 0.05])
 with cent_co:
 
     htp = 'https://github.com/wyattreis/SouthPoleStationFoundation/blob/main/southpole_fromB_cropped.jpg?raw=true'
-    #htp = 'https://github.com/wyattreis/SouthPoleStationFoundation/blob/main/SPS_withFlags.jpg?raw=true'
-    st.image(htp, use_column_width=True, caption='Add caption with credits')
+    st.image(htp, use_column_width=True, caption='U.S. South Pole Station _Credit: Marc Ankenbauer (2013)_')
 
     st.title('South Pole Station Settlement Visualization and Analysis', anchor=False)
 

3a9aa54a38ae4ad09ac6c322b1af2e73775a3b1f
Reis <1623971569121002@mil>
2023-11-15 11:16:54 -0700

    update 3D plot to include slope colors

--- utils.py
+++ utils.py
@@ -1029,7 +1029,7 @@ def plot_3D_settlement_animation(settlementStart, beamInfo3D):
         height = 800,
         annotations = dict(text="Note: Color indicates beam slopes, using the same scale as the plan view of differental slope.",
             x=1, xref="paper", xanchor="right",
-            y=-0.25, yref="paper", yanchor="bottom",
+            y=-0.5, yref="paper", yanchor="bottom",
             align="right", 
             showarrow=False, 
             font = dict(

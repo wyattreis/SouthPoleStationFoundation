6febbe9af815bf21d3356923fc558999e0ca0562
Reis <1623971569121002@mil>
2023-11-16 16:09:08 -0700

    update 3D plot view

--- utils.py
+++ utils.py
@@ -754,7 +754,7 @@ def plot_3D_settlement_slider(settlementStart, beamInfo3D):
     camera = dict(
         up=dict(x=0, y=0, z=1),
         center=dict(x=0, y=0, z=0),
-        eye=dict(x=0, y=5, z=3)
+        eye=dict(x=0, y=4, z=3)
     )
 
     maxSettlement = settlementStart[settlementStart.columns[len(settlementStart.columns)-1]].max()
